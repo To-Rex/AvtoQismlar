2134e8007006393c06e5def088b1cbb935ff9db1
Yorvoration <dev.dilshodjon@gmail.com>
2023-02-09 20:29:39 +0500

    Avto Qismlar

--- lib/fragmentTab/settings_page.dart
+++ lib/fragmentTab/settings_page.dart
@@ -12,7 +12,7 @@ class SettingsPage extends StatefulWidget {
 
 class _SampesPageState extends State<SettingsPage> with SingleTickerProviderStateMixin {
   Color color1 = Colors.indigo;
-  Color color2 = Colors.black87;
+  Color color2 = Colors.black45;
 
   @override
   void initState() {
@@ -31,7 +31,145 @@ class _SampesPageState extends State<SettingsPage> with SingleTickerProviderStat
       ),
       body: Column(
         children: [
-          Text(AppLocalizations.of(context)!.submitInfo),
+          Container(
+            margin: const EdgeInsets.only(top: 10, left: 10, right: 10),
+            child: Row(
+              children: [
+                //location icon and text
+                const Icon(Icons.location_on, color: Colors.indigo),
+                SizedBox(
+                  width: MediaQuery.of(context).size.width * 0.005,
+                ),
+                TextButton(
+                  onPressed: () {
+                  },
+                  child: const Text('Qo`qon',
+                      style: TextStyle(color: Colors.indigo)),
+                ),
+                Expanded(child: Container()),
+                const Icon(Icons.logout, color: Colors.redAccent),
+                SizedBox(
+                  width: MediaQuery.of(context).size.width * 0.005,
+                ),
+                TextButton(
+                  onPressed: () {
+                  },
+                  child: const Text('Logout',
+                      style: TextStyle(color: Colors.redAccent)),
+                ),
+              ],
+            ),
+          ),
+          Container(
+            margin: const EdgeInsets.only(top: 10, left: 10, right: 10),
+            child: Row(
+              children: [
+                Expanded(child: Container()),
+                Container(
+                  width: MediaQuery.of(context).size.width * 0.3,
+                  height: MediaQuery.of(context).size.width * 0.3,
+                  decoration:  BoxDecoration(
+                    color: Colors.indigo,
+                    borderRadius: BorderRadius.all(Radius.circular(MediaQuery.of(context).size.width * 0.5)),
+                  ),
+                  child: Center(
+                    child: Text('A',
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: MediaQuery.of(context).size.width * 0.1,
+                            fontWeight: FontWeight.bold)),
+                  ),
+                ),
+                Expanded(child: Container()),
+              ],
+            ),
+          ),
+          Container(
+            margin: const EdgeInsets.only(top: 10, left: 10, right: 10),
+            child: Row(
+              children: [
+                Expanded(child: Container()),
+                const Text(
+                    'Abdulaziz Abdurahmonov',
+                    style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold)),
+                Expanded(child: Container()),
+              ],
+            ),
+          ),
+          //phone number
+          Container(
+            margin: const EdgeInsets.only(top: 10, left: 10, right: 10),
+            child: Row(
+              children: [
+                Expanded(child: Container()),
+                const Text(
+                    '+998 97 123 45 67',
+                    style: TextStyle(
+                        color: Colors.black54,
+                        fontSize: 15,)),
+                Expanded(child: Container()),
+              ],
+            ),
+          ),
+
+          Expanded(child: Container()),
+          Row(
+            children: [
+              Expanded(child: Container()),
+              Container(
+                width: 20,
+                height: 20,
+                decoration:  BoxDecoration(
+                  color: color1,
+                  //in the form of a circle with a radius of 50
+                  borderRadius: const BorderRadius.all(Radius.circular(13)),
+                ),
+              ),
+              Container(
+                width: MediaQuery.of(context).size.width * 0.3,
+                height: 2,
+                decoration:  BoxDecoration(
+                  color: color1,
+                  //in the form of a circle with a radius of 50
+                  borderRadius: const BorderRadius.all(Radius.circular(13)),
+                ),
+              ),
+              Container(
+                width: 20,
+                height: 20,
+                decoration:  BoxDecoration(
+                  color: color1,
+                  //in the form of a circle with a radius of 50
+                  borderRadius: const BorderRadius.all(Radius.circular(13)),
+                ),
+              ),
+              Container(
+                width: MediaQuery.of(context).size.width * 0.3,
+                height: 2,
+                decoration:  BoxDecoration(
+                  color: color2,
+                  //in the form of a circle with a radius of 50
+                  borderRadius: const BorderRadius.all(Radius.circular(13)),
+                ),
+              ),
+              Container(
+                width: 20,
+                height: 20,
+                decoration:  BoxDecoration(
+                  color: color2,
+                  //in the form of a circle with a radius of 50
+                  borderRadius: const BorderRadius.all(Radius.circular(13)),
+                ),
+              ),
+              Expanded(child: Container()),
+            ],
+          ),
+
+          //name and surname
+
           Expanded(child: Container()),
           Row(
             children: [
@@ -48,9 +186,8 @@ class _SampesPageState extends State<SettingsPage> with SingleTickerProviderStat
                 ),
                 child: TextButton(
                   onPressed: () {
-                    color2 = Colors.black87;
+                    color2 = Colors.black45;
                     color1 = Colors.indigo;
-                    //set lacale
                     MyApp.setLocale(context, const Locale('en'));
                     setState(() {});
                   },
@@ -73,7 +210,7 @@ class _SampesPageState extends State<SettingsPage> with SingleTickerProviderStat
                 child: TextButton(
                   onPressed: () {
                     color2 = Colors.indigo;
-                    color1 = Colors.black87;
+                    color1 = Colors.black45;
                     //set lacale
                     MyApp.setLocale(context, const Locale('ru'));
                     setState(() {});
@@ -88,6 +225,8 @@ class _SampesPageState extends State<SettingsPage> with SingleTickerProviderStat
               Expanded(child: Container()),
             ],
           ),
+          //Logout button
+
           SizedBox(
             height: MediaQuery.of(context).size.height * 0.05,
           ),
