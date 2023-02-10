6411e42bce497d5f21e2ff0ddb4e88e63bcf979e
Yorvoration <dev.dilshodjon@gmail.com>
2023-02-11 00:23:31 +0500

    Avto Qismlar

--- lib/fragmentTab/history_page.dart
+++ lib/fragmentTab/history_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:http/http.dart' as http;
 
 class HistoryPage extends StatefulWidget {
   const HistoryPage({super.key});
@@ -9,9 +11,20 @@ class HistoryPage extends StatefulWidget {
 
 class _SampesPageState extends State<HistoryPage> with SingleTickerProviderStateMixin {
 
+  //function for getting data from shared preferences
+  Future<void> _getSharedPref() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    print(prefs.getString('token'));
+    print(prefs.getString('codeVerified'));
+    print(prefs.getString('fullName'));
+    print(prefs.getString('phone'));
+    print(prefs.getString('address'));
+    print(prefs.getString('client_id'));
+  }
 
   @override
   void initState() {
+    _getSharedPref();
     super.initState();
   }
 
