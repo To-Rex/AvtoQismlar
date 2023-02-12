6d7000e73ad1621d2dd7f1547aee3707f44a0cfb
Yorvoration <dev.dilshodjon@gmail.com>
2023-02-13 03:40:05 +0500

    task

--- lib/fragmentTab/history_page.dart
+++ lib/fragmentTab/history_page.dart
@@ -4,7 +4,6 @@ import 'package:avto_qismlar/models/products.dart';
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:http/http.dart' as http;
-
 import '../models/histry.dart';
 
 class HistoryPage extends StatefulWidget {
@@ -28,32 +27,25 @@ class _SampesPageState extends State<HistoryPage>
   Future<void> _getOrders() async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
     var url = Uri.parse(
-        'http://avtoqismlar.almirab.uz/api/client_orders/${prefs.getString('client_id')}');
+        'http://avtoqismlar.almirab.uz/api/client_orders/${prefs.getString(
+            'client_id')}');
     var response = await http.get(url);
     data = jsonDecode(response.body);
     if (response.statusCode == 200) {
       _orders.clear();
       _orders2.clear();
       for (var i = 0; i < data.length; i++) {
-        var date = data[i]['date']??'';
-        var total = data[i]['total']??'';
-        var status = data[i]['status']??'';
-        var id = data[i]['id']??'';
         _orders.add(HistoryClass(
-            date: date,
-            total: total,
-            status: status,
-            id: id,));
-        /*for (var j = 0; j < data[i]['products'].length; j++) {
-          _orders2.add(ProductClass.fromJson(
-              Map<String, dynamic>.from(data[i]['products'][j])
-          ));
-        }*/
-        /*for(data in data[i]['products']){
-          _orders2.add(ProductClass.fromJson(
-              Map<String, dynamic>.from(data)
-          ));
-        }*/
+            date: data[i]['date'] ?? '',
+            total: data[i]['total'] ?? '',
+            status: data[i]['status'] ?? '',
+            id: data[i]['id'] ?? '',
+            products: [
+              for (var j = 0; j < data[i]['products'].length; j++)
+                ProductClass.fromJson(data[i]['products'][j])
+            ]));
+        print('=====>> ${_orders[i].products.length}');
+        //print('=====>> ${_orders[i].products[0].name}');
       }
       print(_orders.length);
       setState(() {});
@@ -69,16 +61,15 @@ class _SampesPageState extends State<HistoryPage>
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Colors.white,
-        appBar: PreferredSize(
-          preferredSize: const Size.fromHeight(0), // here the desired height
-          child: AppBar(
-            backgroundColor: Colors.indigo,
-          ),
+      backgroundColor: Colors.white,
+      appBar: PreferredSize(
+        preferredSize: const Size.fromHeight(0), // here the desired height
+        child: AppBar(
+          backgroundColor: Colors.indigo,
         ),
-        body: Column(
+      ),
+      body: Column(
         children: [
-          Text('History'),
           Expanded(
             child: ListView.builder(
               itemCount: _orders.length,
@@ -87,15 +78,141 @@ class _SampesPageState extends State<HistoryPage>
                   children: [
                     ListTile(
                       title: Text(_orders[index].date),
-                      trailing: Text(_orders[index].status),
+                      trailing: Text(_orders2.length.toString()),
                     ),
+                    //_orders2.product.length
+                    for (var i = 0; i < _orders[index].products.length; i++)
+                    //cardview for product
+                      GestureDetector(
+                        child: Card(
+                          color: Colors.white,
+                          shadowColor: Colors.grey,
+                          elevation: 5,
+                          margin: const EdgeInsets.all(15),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(15),
+                          ),
+                          child: Row(
+                            children: [
+                              //if (_listProduct[index].picture != '')
+                              if (_orders[index].products[i].picture != '')
+                                Container(
+                                  width: MediaQuery
+                                      .of(context)
+                                      .size
+                                      .width * 0.3,
+                                  height: MediaQuery
+                                      .of(context)
+                                      .size
+                                      .width * 0.3,
+                                  decoration: BoxDecoration(
+                                      borderRadius: BorderRadius.circular(10),
+                                      image: DecorationImage(
+                                          image: NetworkImage(
+                                              'http://avtoqismlar.almirab.uz/public/uploads/products/${_orders[index].products[i].picture}'),
+                                          fit: BoxFit.cover)),
+                                ),
+                              if (_orders[index].products[i].picture == '')
+                                Container(
+                                  width: MediaQuery
+                                      .of(context)
+                                      .size
+                                      .width * 0.3,
+                                  height: MediaQuery
+                                      .of(context)
+                                      .size
+                                      .width * 0.3,
+                                  decoration: BoxDecoration(
+                                      borderRadius: BorderRadius.circular(10),
+                                      image: const DecorationImage(
+                                          image: NetworkImage(
+                                              'https://1gai.ru/uploads/posts/2019-04/1554288456_vfd.jpg'),
+                                          fit: BoxFit.cover)),
+                                ),
+                              SizedBox(
+                                  width:
+                                  MediaQuery
+                                      .of(context)
+                                      .size
+                                      .width * 0.02),
+                              Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  SizedBox(
+                                      height:
+                                      MediaQuery
+                                          .of(context)
+                                          .size
+                                          .height *
+                                          0.01),
+                                  Text(_orders[index].products[i].name,
+                                      style: const TextStyle(
+                                          fontSize: 16,
+                                          fontWeight: FontWeight.bold)),
+                                  SizedBox(
+                                      height:
+                                      MediaQuery
+                                          .of(context)
+                                          .size
+                                          .height *
+                                          0.01),
+                                  Row(
+                                    children: [
+                                      const Text('Brend:',
+                                          style: TextStyle(
+                                            fontSize: 13,
+                                          )),
+                                      Text(
+                                          '${_orders[index].products[i].brand
+                                              .toUpperCase()}',
+                                          style: const TextStyle(
+                                              fontSize: 13,
+                                              color: Colors.orange)),
+                                      SizedBox(
+                                          width: MediaQuery
+                                              .of(context)
+                                              .size
+                                              .width *
+                                              0.01),
+                                      Text('${_orders[index].products[i].country}',
+                                          style: const TextStyle(
+                                            color: Colors.green,
+                                            fontSize: 13,
+                                          )),
+                                    ],
+                                  ),
+                                  SizedBox(
+                                      height:
+                                      MediaQuery
+                                          .of(context)
+                                          .size
+                                          .height *
+                                          0.003),
+                                  SizedBox(
+                                    width: MediaQuery
+                                        .of(context)
+                                        .size
+                                        .width *
+                                        0.61,
+                                  ),
+                                ],
+                              ),
+                            ],
+                          ),
+                        ),
+                        onTap: () {
 
+                        },
+                      ),
                     Divider(
                       endIndent: 10,
                       indent: 10,
                       color: Colors.black54,
                       height: 1,
-                      thickness: MediaQuery.of(context).size.width * 0.002,
+                      thickness: MediaQuery
+                          .of(context)
+                          .size
+                          .width * 0.002,
                     ),
                   ],
                 );
@@ -104,6 +221,6 @@ class _SampesPageState extends State<HistoryPage>
           ),
         ],
       ),
-        );
+    );
   }
 }
--- lib/models/histry.dart
+++ lib/models/histry.dart
@@ -1,21 +1,59 @@
+import 'package:avto_qismlar/models/products.dart';
+
 class HistoryClass {
   String id;
   String date;
   String total;
   String status;
+  List<ProductClass> products = [];
 
-  HistoryClass(
-      {required this.id,
-      required this.date,
-      required this.total,
-      required this.status});
+  HistoryClass({
+    required this.id,
+    required this.date,
+    required this.total,
+    required this.status,
+    required this.products,
+  });
 
   factory HistoryClass.fromJson(Map<String, dynamic> json) {
     return HistoryClass(
-      id: json['id'] ?? '',
-      date: json['date'] ?? '',
-      total: json['total'] ?? '',
-      status: json['status'] ?? '',
+      id: json['id']??'',
+      date: json['date']??'',
+      total: json['total']??'',
+      status: json['status']??'',
+      products: json['products']??[],
     );
   }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'date': date,
+      'total': total,
+      'status': status,
+      'products': products,
+    };
+  }
+
+  @override
+  String toString() {
+    return 'HistoryClass{id: $id, date: $date, total: $total, status: $status, products: $products}';
+  }
+
+  HistoryClass copyWith({
+    String? id,
+    String? date,
+    String? total,
+    String? status,
+    List<ProductClass>? products,
+  }) {
+    return HistoryClass(
+      id: id ?? this.id,
+      date: date ?? this.date,
+      total: total ?? this.total,
+      status: status ?? this.status,
+      products: products ?? this.products,
+    );
+  }
+
 }
