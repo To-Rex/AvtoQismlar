22028c583eec2a8a807aea15383d97af0f3fe7ad
Yorvoration <dev.dilshodjon@gmail.com>
2023-02-12 15:58:23 +0500

    task

--- lib/fragmentTab/history_page.dart
+++ lib/fragmentTab/history_page.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 
+import 'package:avto_qismlar/models/products.dart';
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:http/http.dart' as http;
@@ -17,6 +18,7 @@ class _SampesPageState extends State<HistoryPage>
     with SingleTickerProviderStateMixin {
   var _orders = [];
   var _orders2 = [];
+  var data;
 
   Future<void> _getSharedPref() async {
     SharedPreferences? prefs = await SharedPreferences.getInstance();
@@ -28,20 +30,33 @@ class _SampesPageState extends State<HistoryPage>
     var url = Uri.parse(
         'http://avtoqismlar.almirab.uz/api/client_orders/${prefs.getString('client_id')}');
     var response = await http.get(url);
-    var data = jsonDecode(response.body);
+    data = jsonDecode(response.body);
     if (response.statusCode == 200) {
+      _orders.clear();
+      _orders2.clear();
       for (var i = 0; i < data.length; i++) {
-        var date = data[i]['date'];
-        var total = data[i]['total'];
-        var status = data[i]['status'];
-        var id = data[i]['id'];
+        var date = data[i]['date']??'';
+        var total = data[i]['total']??'';
+        var status = data[i]['status']??'';
+        var id = data[i]['id']??'';
         _orders.add(HistoryClass(
             date: date,
             total: total,
             status: status,
             id: id,));
+        /*for (var j = 0; j < data[i]['products'].length; j++) {
+          _orders2.add(ProductClass.fromJson(
+              Map<String, dynamic>.from(data[i]['products'][j])
+          ));
+        }*/
+        /*for(data in data[i]['products']){
+          _orders2.add(ProductClass.fromJson(
+              Map<String, dynamic>.from(data)
+          ));
+        }*/
       }
       print(_orders.length);
+      setState(() {});
     }
   }
 
@@ -61,38 +76,34 @@ class _SampesPageState extends State<HistoryPage>
             backgroundColor: Colors.indigo,
           ),
         ),
-        body: Center(
-          child: Text('History'),
-        )
-        /*body: Column(
+        body: Column(
         children: [
-          //text Tarix
-          Container(
-            margin: const EdgeInsets.only(top: 10, left: 10, right: 10),
-            child: const Text('Tarix',
-                style: TextStyle(
-                    color: Colors.black,
-                    fontSize: 22,
-                    fontWeight: FontWeight.bold)),
-          ),
-          Container(
-            margin: const EdgeInsets.only(top: 20, left: 20, right: 20),
-            height: MediaQuery.of(context).size.height * 0.20,
+          Text('History'),
+          Expanded(
             child: ListView.builder(
-              scrollDirection: Axis.horizontal,
-              //itemCount: _similars.length,
+              itemCount: _orders.length,
               itemBuilder: (context, index) {
                 return Column(
                   children: [
+                    ListTile(
+                      title: Text(_orders[index].date),
+                      trailing: Text(_orders[index].status),
+                    ),
 
+                    Divider(
+                      endIndent: 10,
+                      indent: 10,
+                      color: Colors.black54,
+                      height: 1,
+                      thickness: MediaQuery.of(context).size.width * 0.002,
+                    ),
                   ],
                 );
               },
             ),
           ),
-
         ],
-      ),*/
+      ),
         );
   }
 }
